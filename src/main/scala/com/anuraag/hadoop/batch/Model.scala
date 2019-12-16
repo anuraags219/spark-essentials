f56c91cc503a33c1e511ef1abbe9470f39946c70
anuraags219 <anuraags219@gmail.com>
2019-12-16 09:07:48 +0530

    Added the zipped data

--- src/main/scala/com/anuraag/hadoop/batch/Model.scala
+++ src/main/scala/com/anuraag/hadoop/batch/Model.scala
@@ -9,7 +9,8 @@ object Model {
   val inputPath = "/home/anuraag/Downloads/stackexchange/christianity.meta.stackexchange.com/Badges.xml"
   val outputPath = "hdfs://localhost:9000/user/anuraag/hadoop_project/output/"
   val fileNames: Seq[String] = Seq("Badges", "Comments", "PostHistory", "PostLinks", "Posts", "Tags", "Users", "Votes")
-  val rootPath = "/home/anuraag/Downloads/stackexchange/christianity.meta.stackexchange.com/"
+  val metaRootPath = "/home/anuraag/Downloads/stackexchange/christianity.meta.stackexchange.com/"
+  val completeRootPath = "/home/anuraag/Downloads/stackexchange/christianity.stackexchange.com/"
   val extensions = List("xml")
 
   def execute(executionContext: ExecutionContext): Unit = {
@@ -20,21 +21,31 @@ object Model {
 //    dataFrames.foreach(dataFrame => println(dataFrame._1))
 //    dataFrames("Badges").show(5, false)
 //    Regex.regexGeneral()
-    val files = BulkTransformations.getListOfFiles(new File(rootPath), extensions)
-    val dataFrames = BulkTransformations.readAllDataFrames(executionContext, files)
-//    dataFrames.foreach(dataFrame => {
-//      println(dataFrame._1)
-//      dataFrame._2.show(1, false)
+    val metaFiles = BulkTransformations.getListOfFiles(new File(metaRootPath), extensions)
+    val metaDataFrames = BulkTransformations.readAllDataFrames(executionContext, metaFiles)
+    println("Meta")
+    metaDataFrames.foreach(dataFrame => {
+      println(dataFrame._1)
+      println(dataFrame._2.count())
 //      dataFrame._2.printSchema()
-//    })
+    })
+
+    val completeFiles = BulkTransformations.getListOfFiles(new File(completeRootPath), extensions)
+    val completeDataFrames = BulkTransformations.readAllDataFrames(executionContext, completeFiles)
+    println("Complete")
+    completeDataFrames.foreach(dataFrame => {
+      println(dataFrame._1)
+      println(dataFrame._2.count())
+      //      dataFrame._2.printSchema()
+    })
     //    files.foreach(file => println(file._1))
 
-    val posts = DataFrameTransformations.getPostsData(executionContext, dataFrames)
+//    val posts = DataFrameTransformations.getPostsData(executionContext, metaDataFrames)
 //    posts.show()
 
-    val badges = DataFrameTransformations.getBadges(executionContext, dataFrames)
-    val joinedDF = badges.join(posts, Seq("_Id"), "left")
-    joinedDF.show()
+//    val badges = DataFrameTransformations.getBadges(executionContext, metaDataFrames)
+//    val joinedDF = badges.join(posts, Seq("_Id"), "left")
+//    joinedDF.show()
 
   }
 }
